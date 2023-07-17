bdfe293fc8154288fd1a0fada25c777dfb7dfcaf
artemplv <artemplv@gmail.com>
2023-07-17 13:37:14 -0400

    fix Folder

--- Refactor/src/components/Folder.js
+++ Refactor/src/components/Folder.js
@@ -37,7 +37,7 @@ const Folder = (props) => {
     setCurrentTab(num);
   }
 
-  const folder = folders[this.state.currentTab];
+  const folder = folders[currentTab];
   const titles = folders.map((folder) => folder.title);
 
   return (
