8b9cab5167deb2313e2d23a2d7a915d33298eaac
artemplv <artemplv@gmail.com>
2023-07-17 14:53:11 -0400

    partly complete phase 5

--- Refactor/src/components/Autocomplete.js
+++ Refactor/src/components/Autocomplete.js
@@ -1,5 +1,83 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+const Autocomplete = (props) => {
+  const inputRef = useRef();
+  const [inputVal, setInputVal] = useState('');
+  const [showList, setShowList] = useState(false);
+
+  useEffect (()=> {
+    if (showList)
+      document.addEventListener('click', handleOutsideClick);
+    else {
+      console.log("Removing Autocomplete listener on update!");
+      document.removeEventListener('click', handleOutsideClick);
+    }
+      
+    return (() => {
+      console.log("Cleaning up event listener from Autocomplete!");
+      document.removeEventListener('click', handleOutsideClick)
+    });
+  }, [showList]);
+
+  const handleInput = (e) => {
+    setInputVal(e.target.value);
+  }
+
+  const selectName = e => {
+    e.stopPropagation();
+    setInputVal(e.target.innerHTML);
+    setShowList(false);
+  }
+
+  handleOutsideClick = () => {
+    // Leave dropdown visible as long as input is focused
+    if (document.activeElement === this.inputRef.current) return;
+    else this.setState({ showList: false });
+  }
+
+  matches = () => {
+    const { inputVal } = this.state;
+    const { names } = this.props;
+    const inputLength = inputVal.length;
+    const matches = [];
+
+    if (inputLength === 0) return names;
+
+    names.forEach(name => {
+      const nameSegment = name.slice(0, inputLength);
+      if (nameSegment.toLowerCase() === inputVal.toLowerCase()) {
+        matches.push(name);
+      }
+    });
+
+    if (matches.length === 0) matches.push('No matches');
+
+    return matches;
+  }
+
+  return (
+    <section className="autocomplete-section">
+      <h1>Autocomplete</h1>
+      <div className="auto">
+        <input
+          placeholder="Search..."
+          ref={inputRef}
+          onChange={handleInput}
+          value={inputVal}
+          onFocus={() => setShowList(true)}
+        />
+        {showList && (
+          <ul className="auto-dropdown">
+            <TransitionGroup>
+              {results}
+            </TransitionGroup>
+          </ul>
+        )}
+      </div>
+    </section>
+  );
+};
 
 class Autocomplete extends React.Component {
   constructor(props) {
